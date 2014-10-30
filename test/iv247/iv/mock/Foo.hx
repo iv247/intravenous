96ce47b959ea0929f7414794541bdc5e15307901
Clark Jones <clark@iv247.com>
2014-10-30 06:50:15 -0400

    set static value on instantiation

--- test/iv247/iv/mock/Foo.hx
+++ test/iv247/iv/mock/Foo.hx
@@ -1,7 +1,11 @@
 package iv247.iv.mock;
 
 class Foo implements IMockObject {
+
+    public static var instantiated : Bool;
+
     public function new(){
 
+        Foo.instantiated = true;
     }
 }
