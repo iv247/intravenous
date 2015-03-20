6157333ba63f30ba74b2046ce30a594d28a3f6d3
Clark Jones <clark@iv247.com>
2015-03-19 23:40:43 -0400

    remove test code

--- test/TestMain.hx
+++ test/TestMain.hx
@@ -4,30 +4,11 @@ import buddy.Buddy;
 import buddy.BuddySuite;
 
 
-/**
-test
-**/
-class TestMain extends BuddySuite implements Buddy {
-/** test; **/
-	public function new() {
-		super();
 
-		var inj:iv247.intravenous.ioc.internal.Injectable<Enum<Dynamic>,Class<Dynamic>> = "One";
-			trace( inj.isClass() );
-		
-	
-	
+class TestMain extends BuddySuite implements Buddy {
+
+	public function new() {
+		super();	
 	}
 
-}
-
-
-enum Testing {
-	ONE;
-	TWO;
-	THREE;
-}
-
-class One {
-	public var test:String;
 }
