894299c3a11f1af4a6a432834d06b12baa9d1930
Clark Jones <clark@iv247.com>
2015-05-23 17:09:24 -0400

    wip

--- src/iv247/intravenous/view/DomView.hx
+++ src/iv247/intravenous/view/DomView.hx
@@ -3,61 +3,60 @@ package iv247.intravenous.view;
 
 import js.Browser;
 
-class DomView implements View
-{
-	public var parent(default,null):View;
+class DomView implements View {
+    public var parent(default, null):View;
 
-	public var children(default,null):Array<View>;
+    public var children(default, null):Array<View>;
 
-	public var viewElement(default,null) : js.html.Element;
+    public var viewElement(default, null):js.html.Element;
 
-	private var nodeType:String;
+    private var nodeType:String;
 
     @dispatcher
-	public var dispatch:Dynamic->Void;
-	
-	public function new(type:String = "div"){
-		this.nodeType = type;
-	}
+    public var dispatch:Dynamic -> Void;
 
-	public function add(view:View) : Void {	
-		if(children == null){
-			children = [];
-		} 
+    public function new (type:String = "div") {
+        this.nodeType = type;
+    }
 
-		if(!Lambda.has(children,view)){
-			this.children.push(view);
-			view.create();
-			view.render();
-			view.onRenderComplete();
-		 	viewElement.appendChild(view.viewElement);
-		}
-	
-		dispatch(new ViewMessage(ViewMessage.Types.ADDED));
-	}
+    public function add (view:View):Void {
+        if ( children == null ) {
+            children = [];
+        }
 
-	public function create():Void {
-		var attr  = Browser.document.createAttribute('view');
-		var type = Type.getClass(this);
-		var className = Type.getClassName(type);
-		var name = untyped type.__name__[type.__name__.length -1];
+        if ( !Lambda.has( children, view ) ) {
+            this.children.push( view );
+            view.create( );
+            view.render( );
+            view.onRenderComplete( );
+            viewElement.appendChild( view.viewElement );
+        }
 
-		attr.value = Type.getClassName( type );
-		viewElement = Browser.document.createElement(nodeType);
-		viewElement.classList.add( name );
-		viewElement.attributes.setNamedItem(attr);
-	}
+        dispatch( new ViewMessage(ViewMessage.Types.ADDED) );
+    }
 
-	public function render() : Void {}
-	public function onRenderComplete() : Void {}
+    public function create ():Void {
+        var attr = Browser.document.createAttribute( 'view' );
+        var type = Type.getClass( this );
+        var className = Type.getClassName( type );
+        var name = untyped type.__name__[type.__name__.length - 1];
 
+        attr.value = Type.getClassName( type );
+        viewElement = Browser.document.createElement( nodeType );
+        viewElement.classList.add( name );
+        viewElement.attributes.setNamedItem( attr );
+    }
 
-	public function remove(view:View) : Void {
-		if(children.remove(view)){
-			view.viewElement.remove();
-		}
+    public function render ():Void {}
 
-		dispatch(new ViewMessage(ViewMessage.Types.REMOVED));
-	}
+    public function onRenderComplete ():Void {}
+
+    public function remove (view:View):Void {
+        if ( children.remove( view ) ) {
+            view.viewElement.remove( );
+        }
+
+        dispatch( new ViewMessage(ViewMessage.Types.REMOVED) );
+    }
 
 }#end
